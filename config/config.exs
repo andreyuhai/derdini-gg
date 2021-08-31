431df93101b85caeb2bed8a2f802dd8768e131ea
andreyuhai <burakaymakci@gmail.com>
2021-08-31 18:13:16 +0200

    Add Guardian config

--- config/config.exs
+++ config/config.exs
@@ -46,6 +46,11 @@ config :ueberauth, Ueberauth,
     ]}
   ]
 
+# Guardian config
+config :derdini_gg, DerdiniGGWeb.Authentication,
+  issuer: "derdini_gg",
+  secret_key: System.get_env("GUARDIAN_SECRET_KEY")
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
