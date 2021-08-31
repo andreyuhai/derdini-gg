2f2ba29f5636f48c841bcb1ed732489442ada583
andreyuhai <burakaymakci@gmail.com>
2021-08-31 18:09:22 +0200

    After successful account creation log the user in and redirect to '/'

--- apps/derdini_gg_web/lib/derdini_gg_web/controllers/registration_controller.ex
+++ apps/derdini_gg_web/lib/derdini_gg_web/controllers/registration_controller.ex
@@ -14,7 +14,9 @@ defmodule DerdiniGGWeb.RegistrationController do
   def create(%{assigns: %{ueberauth_auth: auth_params}} = conn, _params) do
     case Accounts.register(auth_params) do
       {:ok, account} ->
-        redirect(conn, to: Routes.profile_path(conn, :show))
+        conn
+        |> Authentication.log_in(account)
+        |> redirect(to: Routes.page_path(conn, :index))
 
       {:error, changeset} ->
         render(conn, :new,
