029f473a89512a4afb5214d014eddafbc34cb417
andreyuhai <burakaymakci@gmail.com>
2021-08-31 18:08:49 +0200

    Fix typo uberauth -> ueberauth

--- apps/derdini_gg_web/lib/derdini_gg_web/controllers/registration_controller.ex
+++ apps/derdini_gg_web/lib/derdini_gg_web/controllers/registration_controller.ex
@@ -11,7 +11,7 @@ defmodule DerdiniGGWeb.RegistrationController do
     )
   end
 
-  def create(%{assigns: %{uberauth_auth: auth_params}} = conn, _params) do
+  def create(%{assigns: %{ueberauth_auth: auth_params}} = conn, _params) do
     case Accounts.register(auth_params) do
       {:ok, account} ->
         redirect(conn, to: Routes.profile_path(conn, :show))
