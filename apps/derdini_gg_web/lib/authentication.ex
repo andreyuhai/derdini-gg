c6c77136579ad4a9f485c098acb3cab6724a2d96
andreyuhai <burakaymakci@gmail.com>
2021-09-02 22:40:33 +0200

    Implement authenticate/2 and log_out/1 functions

--- apps/derdini_gg_web/lib/authentication.ex
+++ apps/derdini_gg_web/lib/authentication.ex
@@ -21,7 +21,31 @@ defmodule DerdiniGGWeb.Authentication do
     __MODULE__.Plug.sign_in(conn, account)
   end
 
+  def log_out(conn) do
+    __MODULE__.Plug.sign_out(conn)
+  end
+
   def get_current_account(conn) do
     __MODULE__.Plug.current_resource(conn)
+  end
+
+  def authenticate(%Account{} = account, password) do
+    authenticate(
+      account,
+      password,
+      Argon2.verify_pass(password, account.encrypted_password)
+    )
+  end
+
+  def authenticate(nil, password) do
+    authenticate(nil, password, Argon2.no_user_verify())
+  end
+
+  defp authenticate(account, _password, true) do
+    {:ok, account}
+  end
+
+  defp authenticate(_account, _password, false) do
+    {:error, :invalid_credentials}
   end
 end
