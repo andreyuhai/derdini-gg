e2eba741c20e7e9e22c8f05e895a3c6d1b3defe2
andreyuhai <burakaymakci@gmail.com>
2021-10-10 23:31:23 +0200

    Add Corsica as dependency for CORS requests from within the Chrome extension

--- apps/derdini_gg_web/mix.exs
+++ apps/derdini_gg_web/mix.exs
@@ -37,6 +37,7 @@ defmodule DerdiniGGWeb.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:corsica, "~> 1.1"},
       {:phoenix, "~> 1.5.11"},
       {:phoenix_ecto, "~> 4.4"},
       {:phoenix_html, "~> 2.11"},
