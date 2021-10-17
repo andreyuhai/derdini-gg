3cece48a5502552a197365b626deb8d683ea78da
andreyuhai <burakaymakci@gmail.com>
2021-10-17 19:31:53 +0200

    Rename likes -> favorite_count, timestamps type :utc_datetime

--- apps/dert_gg/priv/repo/migrations/20210925162100_create_entries.exs
+++ apps/dert_gg/priv/repo/migrations/20210925162100_create_entries.exs
@@ -4,13 +4,16 @@ defmodule DertGG.Repo.Migrations.CreateEntries do
   def change do
     create table(:entries) do
       add :author, :string
+      add :author_id, :integer
       add :content, :text
-      add :likes, :integer
       add :entry_id, :integer
       add :entry_timestamp, :string
+      add :favorite_count, :integer
+      add :topic_uri, :string
 
-      timestamps()
+      timestamps(type: :utc_datetime)
     end
 
+    create unique_index(:entries, :entry_id)
   end
 end
