ea2086deac96c4b9b240bd7018851349273f81e5
andreyuhai <burakaymakci@gmail.com>
2021-10-17 19:33:42 +0200

    Use ExMachina and fix tests

--- apps/dert_gg/test/dert_gg/entries_test.exs
+++ apps/dert_gg/test/dert_gg/entries_test.exs
@@ -1,71 +1,68 @@
 defmodule DertGG.EntriesTest do
-  use DertGG.DataCase
+  use DertGG.DataCase, async: true
 
   alias DertGG.Entries
+  alias DertGG.Factory
 
   describe "entries" do
     alias DertGG.Entries.Entry
 
-    @valid_attrs %{author: "some author", content: "some content", entry_id: 42, entry_timestamp: "some entry_timestamp", likes: 42}
-    @update_attrs %{author: "some updated author", content: "some updated content", entry_id: 43, entry_timestamp: "some updated entry_timestamp", likes: 43}
-    @invalid_attrs %{author: nil, content: nil, entry_id: nil, entry_timestamp: nil, likes: nil}
-
-    def entry_fixture(attrs \\ %{}) do
-      {:ok, entry} =
-        attrs
-        |> Enum.into(@valid_attrs)
-        |> Entries.create_entry()
-
-      entry
-    end
-
     test "list_entries/0 returns all entries" do
-      entry = entry_fixture()
+      entry = Factory.insert(:entry)
       assert Entries.list_entries() == [entry]
     end
 
     test "get_entry!/1 returns the entry with given id" do
-      entry = entry_fixture()
+      entry = Factory.insert(:entry)
       assert Entries.get_entry!(entry.id) == entry
     end
 
     test "create_entry/1 with valid data creates a entry" do
-      assert {:ok, %Entry{} = entry} = Entries.create_entry(@valid_attrs)
-      assert entry.author == "some author"
-      assert entry.content == "some content"
-      assert entry.entry_id == 42
-      assert entry.entry_timestamp == "some entry_timestamp"
-      assert entry.likes == 42
+      entry_params = Factory.params_for(:entry)
+
+      assert {:ok, %Entry{} = entry} = Entries.create_entry(entry_params)
+      assert entry.author == entry_params.author
+      assert entry.content == entry_params.content
+      assert entry.entry_id == entry_params.entry_id
+      assert entry.entry_timestamp == entry_params.entry_timestamp
+      assert entry.favorite_count == entry_params.favorite_count
     end
 
     test "create_entry/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Entries.create_entry(@invalid_attrs)
+      invalid_entry_params = Factory.params_for(:entry, author: nil)
+
+      assert {:error, %Ecto.Changeset{}} = Entries.create_entry(invalid_entry_params)
     end
 
     test "update_entry/2 with valid data updates the entry" do
-      entry = entry_fixture()
-      assert {:ok, %Entry{} = entry} = Entries.update_entry(entry, @update_attrs)
-      assert entry.author == "some updated author"
-      assert entry.content == "some updated content"
-      assert entry.entry_id == 43
-      assert entry.entry_timestamp == "some updated entry_timestamp"
-      assert entry.likes == 43
+      entry = Factory.insert(:entry)
+      entry_update_params = Factory.params_for(:entry)
+
+      assert {:ok, %Entry{} = entry} = Entries.update_entry(entry, entry_update_params)
+      assert entry.author == entry_update_params.author
+      assert entry.content == entry_update_params.content
+      assert entry.entry_id == entry_update_params.entry_id
+      assert entry.entry_timestamp == entry_update_params.entry_timestamp
+      assert entry.favorite_count == entry_update_params.favorite_count
     end
 
     test "update_entry/2 with invalid data returns error changeset" do
-      entry = entry_fixture()
-      assert {:error, %Ecto.Changeset{}} = Entries.update_entry(entry, @invalid_attrs)
+      entry = Factory.insert(:entry)
+      invalid_entry_params = %{entry_id: nil}
+
+      assert {:error, %Ecto.Changeset{}} = Entries.update_entry(entry, invalid_entry_params)
       assert entry == Entries.get_entry!(entry.id)
     end
 
     test "delete_entry/1 deletes the entry" do
-      entry = entry_fixture()
+      entry = Factory.insert(:entry)
+
       assert {:ok, %Entry{}} = Entries.delete_entry(entry)
       assert_raise Ecto.NoResultsError, fn -> Entries.get_entry!(entry.id) end
     end
 
     test "change_entry/1 returns a entry changeset" do
-      entry = entry_fixture()
+      entry = Factory.insert(:entry)
       assert %Ecto.Changeset{} = Entries.change_entry(entry)
     end
   end
