8a8457e1040cf0ba5687daff808048eea91f2793
andreyuhai <burakaymakci@gmail.com>
2021-11-15 22:17:01 +0100

    Change the Entry schema to have the new fields html_content and text_content, create a module variable fields so that we can pass it to cast and validate_required

--- apps/dert_gg/lib/dert_gg/entries/entry.ex
+++ apps/dert_gg/lib/dert_gg/entries/entry.ex
@@ -6,38 +6,31 @@ defmodule DertGG.Entries.Entry do
   schema "entries" do
     field :author, :string
     field :author_id, :integer
-    field :content, :string
+    field :html_content, :string
+    field :text_content, :string
     field :entry_id, :integer
     field :entry_timestamp, :string
+    field :entry_created_at, :utc_datetime
+    field :entry_updated_at, :utc_datetime
     field :favorite_count, :integer
-    field :topic_uri, :string
+    field :topic_url, :string
 
     has_many :votes, DertGG.Votes.Vote
 
     timestamps(type: :utc_datetime)
   end
 
+  @fields [
+    :author, :author_id, :html_content, :text_content,
+    :entry_id, :entry_timestamp, :entry_created_at,
+    :entry_updated_at, :favorite_count, :topic_url
+  ]
+
   @doc false
   def changeset(entry, attrs) do
     entry
-    |> cast(attrs, [
-      :author,
-      :author_id,
-      :content,
-      :entry_id,
-      :entry_timestamp,
-      :favorite_count,
-      :topic_uri
-    ])
-    |> validate_required([
-      :author,
-      :author_id,
-      :content,
-      :entry_id,
-      :entry_timestamp,
-      :favorite_count,
-      :topic_uri
-    ])
+    |> cast(attrs, @fields)
+    |> validate_required(@fields)
     |> unique_constraint(:entry_id)
   end
 end
