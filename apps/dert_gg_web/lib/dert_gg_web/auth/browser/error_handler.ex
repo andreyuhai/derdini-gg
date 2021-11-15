9278473ed31cb719ee8047ac2d573816e697d8b7
andreyuhai <burakaymakci@gmail.com>
2021-11-15 23:14:11 +0100

    Prefix _ unused variables

--- apps/dert_gg_web/lib/dert_gg_web/auth/browser/error_handler.ex
+++ apps/dert_gg_web/lib/dert_gg_web/auth/browser/error_handler.ex
@@ -4,13 +4,13 @@ defmodule DertGGWeb.Authentication.Browser.ErrorHandler do
   @behaviour Guardian.Plug.ErrorHandler
 
   @impl Guardian.Plug.ErrorHandler
-  def auth_error(conn, {:unauthenticated, reason}, _opts) do
+  def auth_error(conn, {:unauthenticated, _reason}, _opts) do
     conn
     |> put_flash(:error, "You need to be logged in.")
     |> redirect(to: Routes.session_path(conn, :new))
   end
 
-  def auth_error(conn, {type, reason}, _opts) do
+  def auth_error(conn, {_type, _reason}, _opts) do
     conn
     |> put_flash(:error, "Something went wrong.")
     |> redirect(to: Routes.session_path(conn, :new))
