d25f8f637b83f54c8d89cf7964173adb915829b1
andreyuhai <burakaymakci@gmail.com>
2021-11-15 23:13:42 +0100

    Change otp_app name to be dert_gg_web instead of dert_gg

--- apps/dert_gg_web/lib/dert_gg_web/auth/api/pipeline.ex
+++ apps/dert_gg_web/lib/dert_gg_web/auth/api/pipeline.ex
@@ -1,6 +1,6 @@
 defmodule DertGGWeb.Authentication.Api.Pipeline do
   use Guardian.Plug.Pipeline,
-    otp_app: :dert_gg,
+    otp_app: :dert_gg_web,
     error_handler: DertGGWeb.Authentication.Api.ErrorHandler,
     module: DertGGWeb.Authentication
 
