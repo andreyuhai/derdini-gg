88978a7492a4634785f8525a1019d31cc4fc73aa
andreyuhai <burakaymakci@gmail.com>
2021-11-15 23:11:50 +0100

    Add migration for creating votes table

---
+++ apps/dert_gg/priv/repo/migrations/20211015215035_create_votes_table.exs
@@ -0,0 +1,14 @@
+defmodule DertGG.Repo.Migrations.CreateVotesTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:votes) do
+      add :entry_id, :integer
+      add :account_id, :integer
+
+      timestamps(type: :utc_datetime)
+    end
+
+    create unique_index(:votes, [:entry_id, :account_id])
+  end
+end
