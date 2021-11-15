3119862376ab4b452c77f358ff80c55fc7a1ae96
andreyuhai <burakaymakci@gmail.com>
2021-11-16 00:01:05 +0100

    Set HTTP status code 401 after unauth

--- apps/dert_gg_web/lib/dert_gg_web/auth/api/error_handler.ex
+++ apps/dert_gg_web/lib/dert_gg_web/auth/api/error_handler.ex
@@ -6,16 +6,17 @@ defmodule DertGGWeb.Authentication.Api.ErrorHandler do
   @impl Guardian.Plug.ErrorHandler
   def auth_error(conn, {:unauthenticated, _reason}, _opts) do
     conn
+    |> put_status(401)
     |> json(%{
-      authenticated: false
+      error: "unauthenticated",
     })
   end
 
-  def auth_error(conn, {type, reason}, _opts) do
+  def auth_error(conn, {type, _reason}, _opts) do
     conn
+    |> put_status(401)
     |> json(%{
-      error: type,
-      reason: reason
+      error: type
     })
   end
 end
