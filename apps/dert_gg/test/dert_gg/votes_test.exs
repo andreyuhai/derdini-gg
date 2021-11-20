73d97d74ba45dfd3b417071a42db711c7538c2ab
andreyuhai <burakaymakci@gmail.com>
2021-11-21 00:41:01 +0100

    Test create_vote/1

--- apps/dert_gg/test/dert_gg/votes_test.exs
+++ apps/dert_gg/test/dert_gg/votes_test.exs
@@ -4,17 +4,33 @@ defmodule DertGG.VotesTest do
   alias DertGG.Factory
   alias DertGG.Votes
 
-  test "create_vote/1" do
-    account = Factory.insert(:account)
-    entry_params = Factory.params_for(:entry)
-
-    params = %{
-      entry: entry_params,
-      account: account
+  setup do
+    entry_params = %{
+      author: "ssg",
+      author_id: "8097",
+      html_content: "gitar calmak icin kullanilan minik plastik garip nesne.",
+      text_content: "gitar calmak icin kullanilan minik plastik garip nesne.",
+      entry_id: "1",
+      entry_timestamp: "15.02.1999",
+      favorite_count: "13616",
+      topic_url: "https://eksisozluk.com/pena--31782"
     }
 
-    {:ok, %{vote: vote}} = Votes.create_vote(params)
+    %{entry_params: entry_params}
+  end
 
-    assert vote.account_id == account.id
+  describe "create_vote/1" do
+    test "creates a vote with given account and entry_params", %{entry_params: entry_params} do
+      account = Factory.insert(:account)
+
+      params = %{
+        account: account,
+        entry_params: entry_params
+      }
+
+      {:ok, vote} = Votes.create_vote(params)
+
+      assert vote.account_id == account.id
+    end
   end
 end
