66635a94381fcef9ea5971e5137c546739538dcd
andreyuhai <burakaymakci@gmail.com>
2021-11-20 22:55:52 +0100

    Remove the tests for deleted functions which were not being used yet, test all the possible timestamps that we can get from Eksi sozluk

--- apps/dert_gg/test/dert_gg/entries_test.exs
+++ apps/dert_gg/test/dert_gg/entries_test.exs
@@ -2,81 +2,60 @@ defmodule DertGG.EntriesTest do
   use DertGG.DataCase, async: true
 
   alias DertGG.Entries
+  alias DertGG.Entries.Entry
   alias DertGG.Factory
 
-  describe "entries" do
-    alias DertGG.Entries.Entry
+  @possible_entry_timestamps %{
+    "15.02.1999" => [entry_created_at: ~U[1999-02-15 00:00:00Z], entry_updated_at: nil],
+    "17.12.1999 12:35" => [entry_created_at: ~U[1999-12-17 09:35:00Z], entry_updated_at: nil],
+    "13.06.2001 21:35 ~ 14.09.2009 22:29" => [
+      entry_created_at: ~U[2001-06-13 18:35:00Z],
+      entry_updated_at: ~U[2009-09-14 19:29:00Z]
+    ],
+    "30.07.2002 09:47 ~ 09:50" => [
+      entry_created_at: ~U[2002-07-30 06:47:00Z],
+      entry_updated_at: ~U[2002-07-30 06:50:00Z]
+    ]
+  }
 
-    test "list_entries/0 returns all entries" do
-      entry = Factory.insert(:entry)
-
-      assert Entries.list_entries() == [entry]
-    end
-
-    test "get_entry!/1 returns the entry with given id" do
-      entry = Factory.insert(:entry)
-
-      assert Entries.get_entry!(entry.id) == entry
-    end
-
-    test "create_entry/1 with valid data creates an entry" do
-      params = %{
+  describe "create_entry/1" do
+    setup do
+      entry_params = %{
+        author: "ssg",
+        author_id: "8097",
         html_content: "gitar calmak icin kullanilan minik plastik garip nesne.",
         text_content: "gitar calmak icin kullanilan minik plastik garip nesne.",
-        entry_created_at: ~U[2021-11-15 20:37:00Z],
-        entry_updated_at: ~U[2021-12-15 12:40:00Z],
+        entry_id: "1",
+        entry_timestamp: "15.02.1999",
+        favorite_count: "13616",
         topic_url: "https://eksisozluk.com/pena--31782"
       }
 
-      entry_params = Factory.params_for(:entry, params)
+      %{entry_params: entry_params}
+    end
 
-      assert {:ok, %Entry{} = entry} = Entries.create_entry(entry_params)
-      assert entry.author == entry_params.author
-      assert entry.author_id == entry_params.author_id
-      assert entry.html_content == entry_params.html_content
-      assert entry.text_content == entry_params.text_content
-      assert entry.entry_id == entry_params.entry_id
-      assert entry.entry_timestamp == entry_params.entry_timestamp
-      assert entry.entry_created_at == entry_params.entry_created_at
-      assert entry.entry_updated_at == entry_params.entry_updated_at
-      assert entry.favorite_count == entry_params.favorite_count
-      assert entry.topic_url == entry_params.topic_url
+    for {entry_timestamp,
+         [entry_created_at: entry_created_at, entry_updated_at: entry_updated_at]} <-
+          @possible_entry_timestamps do
+      test "creates entry with entry timestamp #{entry_timestamp}", %{
+        entry_params: entry_params
+      } do
+        entry_params = %{entry_params | entry_timestamp: unquote(entry_timestamp)}
+
+        assert {:ok, %Entry{} = entry} = Entries.create_entry(entry_params)
+
+        assert entry.entry_created_at == unquote(Macro.escape(entry_created_at)),
+               "With given entry_timestamp #{entry_params.entry_timestamp} got entry_created_at as #{entry.entry_created_at}"
+
+        assert entry.entry_updated_at == unquote(Macro.escape(entry_updated_at)),
+               "With given entry_timestamp #{entry_params.entry_timestamp} got entry_updated_at as #{entry.entry_updated_at}"
+      end
     end
 
     test "create_entry/1 with invalid data returns error changeset" do
       invalid_entry_params = Factory.params_for(:entry, author: nil)
 
       assert {:error, %Ecto.Changeset{}} = Entries.create_entry(invalid_entry_params)
-    end
-
-    test "update_entry/2 with valid data updates the entry" do
-      entry = Factory.insert(:entry)
-
-      entry_update_params = %{
-        html_content: "Changed html content",
-        text_content: "Changed text content",
-        favorite_count: 123
-      }
-
-      assert {:ok, %Entry{} = entry} = Entries.update_entry(entry, entry_update_params)
-      assert entry.html_content == entry_update_params.html_content
-      assert entry.text_content == entry_update_params.text_content
-      assert entry.favorite_count == entry_update_params.favorite_count
-    end
-
-    test "update_entry/2 with invalid data returns error changeset" do
-      entry = Factory.insert(:entry)
-      invalid_entry_params = %{entry_id: nil}
-
-      assert {:error, %Ecto.Changeset{}} = Entries.update_entry(entry, invalid_entry_params)
-      assert Entries.get_entry!(entry.id) == entry
-    end
-
-    test "delete_entry/1 deletes the entry" do
-      entry = Factory.insert(:entry)
-
-      assert {:ok, %Entry{}} = Entries.delete_entry(entry)
-      assert_raise Ecto.NoResultsError, fn -> Entries.get_entry!(entry.id) end
     end
 
     test "change_entry/1 returns an entry changeset" do
