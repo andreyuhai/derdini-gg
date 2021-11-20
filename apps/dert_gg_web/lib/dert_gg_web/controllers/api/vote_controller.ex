9af3e9eea9f1e004c21629e5794783592e544aef
andreyuhai <burakaymakci@gmail.com>
2021-11-21 00:42:14 +0100

    Implement prepare_params_for_entry/1, now create/2 action creates a vote if it doesn't exists, if the vote exists then it deletes the vote

--- apps/dert_gg_web/lib/dert_gg_web/controllers/api/vote_controller.ex
+++ apps/dert_gg_web/lib/dert_gg_web/controllers/api/vote_controller.ex
@@ -2,11 +2,31 @@ defmodule DertGGWeb.Api.VoteController do
   use DertGGWeb, :controller
 
   alias DertGGWeb.Authentication
+  alias DertGG.Votes
+  alias DertGG.Votes.Vote
 
-  def upvote(conn, _params) do
-    with _account <- Authentication.get_current_account(conn) do
+  def create(conn, params) do
+    with account <- Authentication.get_current_account(conn) do
+      entry_params = prepare_params_for_entry(params)
+      entry_id = Map.get(entry_params, "entry_id")
+
+      case Votes.get_vote(account.id, entry_id) do
+        %Vote{} = vote ->
+          Votes.delete_vote(vote)
+
+        nil ->
+          {:ok, changes} = Votes.create_vote(%{account: account, entry_params: entry_params})
+      end
+
       conn
       |> json(%{})
     end
+  end
+
+  defp prepare_params_for_entry(params) do
+    params
+    |> Map.new(fn {key, value} ->
+      {String.replace(key, "-", "_"), value}
+    end)
   end
 end
