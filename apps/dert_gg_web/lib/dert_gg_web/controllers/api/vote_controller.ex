c90319b990afe7a9f04e57b725eab91c5b843c92
andreyuhai <burakaymakci@gmail.com>
2021-12-03 01:26:41 +0100

    create/2 action returns the vote count now in JSON response

--- apps/dert_gg_web/lib/dert_gg_web/controllers/api/vote_controller.ex
+++ apps/dert_gg_web/lib/dert_gg_web/controllers/api/vote_controller.ex
@@ -8,25 +8,34 @@ defmodule DertGGWeb.Api.VoteController do
   def create(conn, params) do
     with account <- Authentication.get_current_account(conn) do
       entry_params = prepare_params_for_entry(params)
-      entry_id = Map.get(entry_params, "entry_id")
+      entry_id = Map.get(entry_params, :entry_id)
 
       case Votes.get_vote(account.id, entry_id) do
         %Vote{} = vote ->
           Votes.delete_vote(vote)
 
         nil ->
-          {:ok, changes} = Votes.create_vote(%{account: account, entry_params: entry_params})
+          {:ok, _vote} = Votes.create_vote(%{account: account, entry_params: entry_params})
       end
 
+      vote_count = Votes.get_count(entry_id)
+
       conn
-      |> json(%{})
+      |> json(%{
+        "vote-count": vote_count
+      })
     end
   end
 
   defp prepare_params_for_entry(params) do
     params
     |> Map.new(fn {key, value} ->
-      {String.replace(key, "-", "_"), value}
+      key =
+        key
+        |> String.replace("-", "_")
+        |> String.to_atom()
+
+      {key, value}
     end)
   end
 end
