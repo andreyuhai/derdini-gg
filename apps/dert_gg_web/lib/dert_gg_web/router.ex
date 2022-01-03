1af67de187b0f6b80a91c9eb82c9139177572507
andreyuhai <burakaymakci@gmail.com>
2022-01-03 22:23:01 +0100

    Plug :fetch_live_flash and :put_root_layout for LiveView

--- apps/dert_gg_web/lib/dert_gg_web/router.ex
+++ apps/dert_gg_web/lib/dert_gg_web/router.ex
@@ -4,7 +4,9 @@ defmodule DertGGWeb.Router do
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
-    plug :fetch_flash
+    # plug :fetch_flash
+    plug :fetch_live_flash
+    plug :put_root_layout, {DertGGWeb.LayoutView, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
     plug DertGGWeb.Authentication.Browser.Pipeline
