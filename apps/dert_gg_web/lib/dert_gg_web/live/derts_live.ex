518bb2187b5f0c05d842df72e695002dcd638da2
andreyuhai <burakaymakci@gmail.com>
2022-01-16 00:12:03 +0100

    Remove the render function because we've extracted the HTML to a separate HEEX file

--- apps/dert_gg_web/lib/dert_gg_web/live/derts_live.ex
+++ apps/dert_gg_web/lib/dert_gg_web/live/derts_live.ex
@@ -3,33 +3,6 @@ defmodule DertGGWeb.DertsLive do
 
   alias DertGGWeb.Authentication
 
-  on_mount({DertGGWeb.LiveHelpers.ResourceAssigns, :default})
-
-  def render(assigns) do
-    ~H"""
-    <table>
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Entry</th>
-          <th scope="col">By</th>
-          <th scope="col">GG Count</th>
-        </tr>
-      </thead>
-      <tbody>
-        <%= for {entry, index} <- Enum.with_index(@entries, 1) do %>
-          <tr>
-            <th scope="row"><%= index %></th>
-            <td><a href={"https://eksisozluk.com/entry/#{entry.entry_id}"}>Entry link</a></td>
-            <td><%= entry.author %></td>
-            <td><%= IO.inspect(length(entry.votes)) %></td>
-          </tr>
-        <% end %>
-      </tbody>
-    </table>
-    """
-  end
-
   def mount(_params, session, socket) do
     IO.inspect(session, label: "HERE IS THE SESSION")
     IO.inspect(socket, label: "HERE IS THE SOCKET")
