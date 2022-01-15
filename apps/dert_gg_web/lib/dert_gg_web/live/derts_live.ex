7aafd740530818154c8e519c4599debe55af779d
andreyuhai <burakaymakci@gmail.com>
2022-01-16 00:14:36 +0100

    Remove the account check as this page doesn't need authentication. Leave the bare minimum for the LiveView, meaning no updates yet

--- apps/dert_gg_web/lib/dert_gg_web/live/derts_live.ex
+++ apps/dert_gg_web/lib/dert_gg_web/live/derts_live.ex
@@ -4,24 +4,12 @@ defmodule DertGGWeb.DertsLive do
   alias DertGGWeb.Authentication
 
   def mount(_params, session, socket) do
-    IO.inspect(session, label: "HERE IS THE SESSION")
-    IO.inspect(socket, label: "HERE IS THE SOCKET")
-
-    with account <- Authentication.get_current_account(socket) do
-      entries = DertGG.Entries.get_entries()
-      if connected?(socket), do: Process.send_after(self(), :update, 1000)
-
-      {
-        :ok,
-        socket
-        |> assign(:current_account, account)
-        |> assign(:entries, entries)
-      }
-    end
-  end
-
-  def handle_info(:update, socket) do
     entries = DertGG.Entries.get_entries()
-    {:noreply, assign(socket, entries: entries)}
+
+    {
+      :ok,
+      socket
+      |> assign(:entries, entries)
+    }
   end
 end
