63a1d0e88359ecfe4185a46c427e563fe85b007f
andreyuhai <burakaymakci@gmail.com>
2022-02-06 00:45:10 +0100

    Implement Live.Helpers.AuthHelper to authenticate a user from guardian_default_token

---
+++ apps/dert_gg_web/lib/dert_gg_web/live/helpers/auth_helper.ex
@@ -0,0 +1,15 @@
+defmodule DertGGWeb.Live.Helpers.AuthHelper do
+  @moduledoc false
+
+  import Phoenix.LiveView
+
+  def on_mount(:default, _params, %{"guardian_default_token" => token}, socket) do
+    {:ok, account, _claims} = DertGGWeb.Authentication.resource_from_token(token)
+
+    {:cont, assign(socket, :current_account, account)}
+  end
+
+  def on_mount(:default, _params, _session, socket) do
+    {:cont, socket}
+  end
+end
