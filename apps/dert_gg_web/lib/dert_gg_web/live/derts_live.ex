eb499646d28072011a6e1e11f4948c5556006bd4
andreyuhai <burakaymakci@gmail.com>
2022-06-05 21:23:10 +0200

    Handle events like close-modal and show-modal

--- apps/dert_gg_web/lib/dert_gg_web/live/derts_live.ex
+++ apps/dert_gg_web/lib/dert_gg_web/live/derts_live.ex
@@ -3,16 +3,37 @@ defmodule DertGGWeb.DertsLive do
 
   alias Phoenix.PubSub
 
+  import DertGGWeb.Components.Row
+
   on_mount DertGGWeb.Live.Helpers.AuthHelper
 
   def mount(_params, session, socket) do
     entries = DertGG.Entries.get_top_entries()
     if connected?(socket), do: PubSub.subscribe(DertGGWeb.PubSub, "vote-updates")
 
-    {:ok, assign(socket, entries: entries)}
+    {:ok, assign(socket, entries: entries, show_modal: false)}
   end
 
   def handle_info(%{entries: entries}, socket) do
     {:noreply, assign(socket, :entries, entries)}
+  end
+
+  def handle_event("close-modal", %{} = params, socket) do
+    {:noreply, push_patch(socket, to: "/", show_modal: false)}
+  end
+
+  def handle_event("show-modal", %{} = params, socket) do
+    {:noreply, push_patch(socket, to: "/#{params["entry"]}", show_modal: true)}
+  end
+
+  def handle_params(%{"entry_id" => entry_id}, uri, socket) do
+    %{entry: entry} =
+      Enum.find(socket.assigns.entries, &(&1.entry.id == String.to_integer(entry_id)))
+
+    {:noreply, assign(socket, show_modal: true, entry: entry)}
+  end
+
+  def handle_params(_, _, socket) do
+    {:noreply, assign(socket, show_modal: false)}
   end
 end
