d41b3fce72894cd937fa842e2c612b9b501c8542
andreyuhai <burakaymakci@gmail.com>
2022-06-11 21:23:25 +0200

    Add dert_gg_web release

--- mix.exs
+++ mix.exs
@@ -7,7 +7,12 @@ defmodule DertGG.Umbrella.MixProject do
       version: "0.1.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      aliases: aliases()
+      aliases: aliases(),
+      releases: [
+        dert_gg_web: [
+          applications: [dert_gg_web: :permanent]
+        ]
+      ]
     ]
   end
 
