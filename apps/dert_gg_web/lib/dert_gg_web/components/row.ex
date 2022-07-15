44ce7531dc241f4d2124846d0b137a70b91214b8
andreyuhai <burakaymakci@gmail.com>
2022-07-15 23:10:23 +0200

    General layout changes Translate English to Turkish Remove forgot password link Remove links that point to either sign up or login pages Remove the author name from the table which also improves the mobile view

--- apps/dert_gg_web/lib/dert_gg_web/components/row.ex
+++ apps/dert_gg_web/lib/dert_gg_web/components/row.ex
@@ -6,7 +6,6 @@ defmodule DertGGWeb.Components.Row do
     <tr class="table-hover" phx-click="show-modal" phx-value-entry={@entry.id}>
       <td scope="row"><%= @num %></td>
       <td><div class="entry-content"><%= @entry.text_content %></div></td>
-      <td><a href={"https://eksisozluk.com/entry/#{@entry.entry_id}"}><%= @entry.author %></a></td>
       <td><%= @vote_count %></td>
     </tr>
     """
