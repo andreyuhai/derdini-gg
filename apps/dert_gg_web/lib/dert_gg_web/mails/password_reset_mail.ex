168488c30be9e7d76f8b86f71695082b668143bf
andreyuhai <burakaymakci@gmail.com>
2022-07-17 15:56:21 +0200

    Comment out unused modules, not planning to have this feature yet

--- apps/dert_gg_web/lib/dert_gg_web/mails/password_reset_mail.ex
+++ apps/dert_gg_web/lib/dert_gg_web/mails/password_reset_mail.ex
@@ -1,17 +1,17 @@
-defmodule DertGGWeb.Mails.PasswordResetMail do
-  use Phoenix.Swoosh,
-    template_root: "lib/dert_gg_web/templates",
-    template_path: "mails"
+# defmodule DertGGWeb.Mails.PasswordResetMail do
+#   use Phoenix.Swoosh,
+#     template_root: "lib/dert_gg_web/templates",
+#     template_path: "mails"
 
-  alias DertGGWeb.Router.Helpers, as: Routes
+#   alias DertGGWeb.Router.Helpers, as: Routes
 
-  def new(email, reset_token) do
-    new()
-    |> from("dont-reply@dert.gg")
-    |> to(email)
-    |> subject("Şifre yenileme talebi - Dert GG")
-    |> render_body("password_reset_mail.html", %{
-      reset_link: Routes.password_reset_url(DertGGWeb.Endpoint, :edit, reset_token)
-    })
-  end
-end
+#   def new(email, reset_token) do
+#     new()
+#     |> from("dont-reply@dert.gg")
+#     |> to(email)
+#     |> subject("Şifre yenileme talebi - Dert GG")
+#     |> render_body("password_reset_mail.html", %{
+#       reset_link: Routes.password_reset_url(DertGGWeb.Endpoint, :edit, reset_token)
+#     })
+#   end
+# end
